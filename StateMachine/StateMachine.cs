03182e2c65d3abbe8fd464098df7f0c13fad051e
gdennis <gregsdennis@users.noreply.github.com>
2012-04-28 09:36:24 -0500

    Updated file list.  Added license comments in source files.

--- StateMachine/StateMachine.cs
+++ StateMachine/StateMachine.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿/***************************************************************************************
+
+	Copyright 2012 Greg Dennis
+
+	   Licensed under the Apache License, Version 2.0 (the "License");
+	   you may not use this file except in compliance with the License.
+	   You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
+ 
+	File Name:		StateMachine.cs
+	Namespace:		Manatee.StateMachine
+	Class Name:		StateMachine<TState, TInput>
+	Purpose:		Represents an extensibe generic state machine.
+
+***************************************************************************************/
+using System;
 using System.Collections.Generic;
 using Manatee.StateMachine.Exceptions;
 
 namespace Manatee.StateMachine
 {
 	/// <summary>
-	/// Represents a generic finite state machine.
+	/// Represents an extensibe generic state machine.
 	/// </summary>
 	/// <typeparam name="TState">The object type to be used for states.</typeparam>
 	/// <typeparam name="TInput">The object type to be used for inputs.</typeparam>
