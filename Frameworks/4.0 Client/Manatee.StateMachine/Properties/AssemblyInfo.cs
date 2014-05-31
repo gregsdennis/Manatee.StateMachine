e03a6882b9df8bfc5a1f2dd9853c07ef4d3512f0
gregsdennis <gregsdennis@localhost>
2014-05-30 21:24:29 -0500

    Added projects to support .Net frameworks 3.5+

--- Frameworks/4.0 Client/Manatee.StateMachine/Properties/AssemblyInfo.cs
+++ Frameworks/4.0 Client/Manatee.StateMachine/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e945f3ba-82d7-4b4a-9d88-4b5c357e5426")]
+[assembly: Guid("74af17ba-e617-43bb-ad2b-87602a420aac")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Frameworks/4.5/Manatee.StateMachine/Properties/AssemblyInfo.cs
+++ Frameworks/4.5/Manatee.StateMachine/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e945f3ba-82d7-4b4a-9d88-4b5c357e5426")]
+[assembly: Guid("b4f54d58-bc91-499b-ba2a-68665d6f2069")]
 
 // Version information for an assembly consists of the following four values:
 //
